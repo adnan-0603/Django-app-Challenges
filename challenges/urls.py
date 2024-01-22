1e2983a662f82cd2635da703595c67652e0a11bd
Mohammad Adnan Chowdhury <dev.adnan.ingermany@gmail.com>
2024-01-22 14:07:58 +0100

    path converters

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path("<int:month>", views.monthly_challenge_by_number),
     path("<month>", views.monthly_challenge)
-
 
 
 ]
