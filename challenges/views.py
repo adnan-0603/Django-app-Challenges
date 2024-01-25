88be8c2a0c1a96ec564f0055a85abcc14dcc42a6
Mohammad Adnan Chowdhury <dev.adnan.ingermany@gmail.com>
2024-01-25 21:03:29 +0100

    Dynamic view logic

--- challenges/views.py
+++ challenges/views.py
@@ -4,16 +4,34 @@ from django.urls import reverse
 
 # create your views here
 monthly_challenges = {
-    "april": "I want to read 4 books",
-    "may": "I want to read 5 books",
-    "june": "I want to read 6 books",
-    "july": "I want to read 7 books",
-    "august": "I want to read 8 books",
-    "september": "I want to read 9 books",
-    "october": "I want to read 10 books",
-    "november": "I want to read 11 books",
-    "december": "I want to read 12 books"
+    "january": "Peak",
+    "february": "Mindset",
+    "march": "Deep work",
+    "april": "10 minutes 38 seconds",
+    "may": "how to win friends and influence people",
+    "june": "Atomic habits",
+    "july": "Grit",
+    "august": "Think like a manager",
+    "september": "Seven habits of highly effective people",
+    "october": "mindshift",
+    "november": "Learning how to learn",
+    "december": "Anything you want"
 }
+
+# creat your views here.
+
+
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        captitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{captitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
 
 
 def monthly_challenge_by_number(request, month):
@@ -27,7 +45,7 @@ def monthly_challenge_by_number(request, month):
     return HttpResponseRedirect(redirect_path)
 
 
-"""
+""" 
 def monthly_challenge(request, month):
     challenge_text = None
     if month == "january":
@@ -50,12 +68,3 @@ def monthly_challenge(request, month):
         return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
-
-
-def weekly_challenge(request, day):
-    try:
-        challenge_text = weekly_challenge[day]
-        response_data = f"<h1>{challenge_text}<h1>"
-        return HttpResponse(response_data)
-    except:
-        return HttpResponseNotFound("<h1>This week is not supported!<h1>")
